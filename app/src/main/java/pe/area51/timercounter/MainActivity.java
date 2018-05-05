d288b87958023391eb90c560c7ebe70dd24afa0f
Gims Vilchez <jvilchezncapas@gmail.com>
2018-05-04 21:18:10 -0500

    primeras modificaciones

--- app/src/main/java/pe/area51/timercounter/MainActivity.java
+++ app/src/main/java/pe/area51/timercounter/MainActivity.java
@@ -2,12 +2,16 @@ package pe.area51.timercounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textViewTimeCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textViewTimeCount =findViewById(R.id.textViewTimeCount);
     }
 }
